0424b7940e91b60dcbb8a31b5599c496a362cfde
hhoow0093 <howarddai2005@gmail.com>
2025-09-15 11:04:06 +0700

    Commit No. 1 - Retrofit Setup and Fetch API JSON Response

---
+++ app/src/main/java/eu/tutorials/lab_week_05/MainActivity.kt
@@ -0,0 +1,70 @@
+package eu.tutorials.lab_week_05
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import eu.tutorials.lab_week_05.api.CatApiService
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import android.widget.TextView
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
+class MainActivity : AppCompatActivity() {
+    private val retrofit by lazy{
+        Retrofit.Builder()
+            .baseUrl("https://api.thecatapi.com/v1/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+    }
+
+    private val catApiService by lazy {
+        retrofit.create(CatApiService::class.java)
+    }
+
+    private val apiResponseView: TextView by lazy{
+        findViewById(R.id.api_response)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        getCatImageResponse()
+
+    }
+    private fun getCatImageResponse() {
+        val call = catApiService.searchImages(1, "full")
+        call.enqueue(object: Callback<String> {
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                if (response.isSuccessful) {
+                    apiResponseView.text = response.body()
+                } else {
+                    Log.e(
+                        MAIN_ACTIVITY,
+                        "Failed to get response\n${response.errorBody()?.string().orEmpty()}"
+                    )
+                }
+            }
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(MAIN_ACTIVITY, "Failed to get response", t)
+            }
+        })
+    }
+
+    companion object{
+        const val MAIN_ACTIVITY = "MAIN_ACTIVITY"
+    }
+
+}
